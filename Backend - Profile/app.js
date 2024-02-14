869f218d081b442b7373434c6041069cba00a654
MMagesh23 <91615388+MMagesh23@users.noreply.github.com>
2024-02-14 11:15:37 +0530

    Add files via upload

---
+++ Backend - Profile/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
+
+//Authentications
+app.use("/", authRoutes);
+
+//Profile routes
+app.use("/profile", profileRoutes);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Connected and Listening on port ${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ Backend - Profile/models/User.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  dob: {
+    type: Date,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  phone: {
+    type: Number,
+    min: 10,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  const today = new Date();
+  const birthDate = new Date(this.dob);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+
+  if (
+    monthDiff < 0 ||
+    (monthDiff === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  this.age = age;
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
